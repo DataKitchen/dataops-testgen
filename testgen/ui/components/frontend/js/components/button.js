9d8be9b359dffbf3331435055b3bb3885d7b9c26
Luis Trinidad <ltrinidad@datakitchen.io>
2024-08-29 21:19:23 -0400

    misc(ui): add reusable component for custom buttons

--- testgen/ui/components/frontend/js/components/button.js
+++ testgen/ui/components/frontend/js/components/button.js
@@ -1,36 +1,65 @@
 /**
  * @typedef Properties
  * @type {object}
+ * @property {(string)} type
+ * @property {(string|null)} label
  * @property {(string|null)} icon
- * @property {(string|null)} class
+ * @property {(string|null)} tooltip
+ * @property {(string|null)} tooltipPosition
  * @property {(Function|null)} onclick
  */
+import { enforceElementWidth } from '../utils.js';
 import van from '../van.min.js';
 import { Streamlit } from '../streamlit.js';
 
 const { button, i, span } = van.tags;
+const BUTTON_TYPE = {
+    BASIC: 'basic',
+    FLAT: 'flat',
+    ICON: 'icon',
+    STROKED: 'stroked',
+};
 
-const Button = (/** @type Properties */ props, /** @type string */ text) => {
-    Streamlit.setFrameHeight();
+const Button = (/** @type Properties */ props) => {
+    Streamlit.setFrameHeight(40);
+
+    const isIconOnly = props.type === BUTTON_TYPE.ICON || (props.icon?.val && !props.label?.val);
+    if (isIconOnly) { // Force a 40px width for the parent iframe & handle window resizing
+        enforceElementWidth(window.frameElement, 40);
+    }
+
+    if (props.tooltip) {
+        window.frameElement.parentElement.setAttribute('data-tooltip', props.tooltip.val);
+        window.frameElement.parentElement.setAttribute('data-tooltip-position', props.tooltipPosition.val);
+    }
 
     if (!window.testgen.loadedStylesheets.button) {
         document.adoptedStyleSheets.push(stylesheet);
         window.testgen.loadedStylesheets.button = true;
     }
 
+    const onClickHandler = props.onclick || post;
     return button(
         {
-            class: `tg-button ${props.icon ? 'tg-icon-button' : ''} ${props.class ?? ''}`,
-            onclick: props.onclick,
+            class: `tg-button tg-${props.type.val}-button ${props.type.val !== 'icon' && isIconOnly ? 'tg-icon-button' : ''}`,
+            onclick: onClickHandler,
         },
+        span({class: 'tg-button-focus-state-indicator'}, ''),
         props.icon ? i({class: 'material-symbols-rounded'}, props.icon) : undefined,
-        span(text),
+        !isIconOnly ? span(props.label) : undefined,
     );
 };
+
+function post() {
+    Streamlit.sendData({ value: Math.random() });
+}
 
 const stylesheet = new CSSStyleSheet();
 stylesheet.replace(`
 button.tg-button {
+    width: 100%;
+    height: 40px;
+
     position: relative;
     overflow: hidden;
 
@@ -41,25 +70,62 @@ button.tg-button {
 
     outline: 0;
     border: unset;
-    background: transparent;
     border-radius: 4px;
-    padding: 8px 16px;
+    padding: 8px 11px;
 
-    color: var(--primary-text-color);
     cursor: pointer;
 
     font-size: 14px;
-
-    transition: background 400ms;
+    color: var(--button-text-color);
+    background: var(--button-basic-background);
 }
 
-button.tg-button:hover {
-    background: rgba(0, 0, 0, 0.04);
+button.tg-button .tg-button-focus-state-indicator::before {
+    content: "";
+    opacity: 0;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: absolute;
+    pointer-events: none;
+    border-radius: inherit;
+    background: var(--button-hover-state-background);
+}
+
+button.tg-button.tg-basic-button {
+    color: var(--button-basic-text-color);
+}
+
+button.tg-button.tg-flat-button {
+    color: var(--button-flat-text-color);
+    background: var(--button-flat-background);
+}
+
+button.tg-button.tg-stroked-button {
+    color: var(--button-stroked-text-color);
+    background: var(--button-stroked-background);
+    border: var(--button-stroked-border);
+}
+
+button.tg-button.tg-icon-button {
+    width: 40px;
+}
+
+button.tg-button:has(span) {
+    padding: 8px 16px;
 }
 
 button.tg-button.tg-icon-button > i {
     font-size: 18px;
+}
+
+button.tg-button > i:has(+ span) {
     margin-right: 8px;
+}
+
+button.tg-button:hover .tg-button-focus-state-indicator::before {
+    opacity: var(--button-hover-state-opacity);
 }
 `);
 
--- testgen/ui/components/widgets/__init__.py
+++ testgen/ui/components/widgets/__init__.py
@@ -5,3 +5,4 @@ from testgen.ui.components.widgets.expander_toggle import expander_toggle
 from testgen.ui.components.widgets.sidebar import sidebar
 from testgen.ui.components.widgets.summary_bar import summary_bar
 from testgen.ui.components.widgets.card import card
+from testgen.ui.components.widgets.button import button
