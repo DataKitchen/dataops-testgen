b6be8741ceef25d34842d9f9b6a7e63deacd13a1
Luis Trinidad <ltrinidad@datakitchen.io>
2024-08-29 21:08:52 -0400

    misc(ui): add custom component for cards

--- testgen/ui/components/widgets/__init__.py
+++ testgen/ui/components/widgets/__init__.py
@@ -4,3 +4,4 @@ from testgen.ui.components.widgets.breadcrumbs import breadcrumbs
 from testgen.ui.components.widgets.expander_toggle import expander_toggle
 from testgen.ui.components.widgets.sidebar import sidebar
 from testgen.ui.components.widgets.summary_bar import summary_bar
+from testgen.ui.components.widgets.card import card
