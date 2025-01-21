e178d1313e19dfc5c8b11fd94f8e27ea01f14d74
Aarthy Adityan <aarthy@datakitchen.io>
2025-01-21 17:54:44 -0500

    feat(score-issues): add pagination, multi-selection and issue reports in score drilldown

--- testgen/ui/queries/scoring_queries.py
+++ testgen/ui/queries/scoring_queries.py
@@ -183,6 +183,7 @@ def get_score_card_issues(
     ),
     anomalies AS (
         SELECT
+            results.id::VARCHAR AS id,
             results.table_name AS table,
             results.column_name AS column,
             types.anomaly_name AS type,
@@ -216,6 +217,7 @@ def get_score_card_issues(
     ),
     tests AS (
         SELECT
+            test_results.id::VARCHAR AS id,
             test_results.table_name AS table,
             test_results.column_names AS column,
             test_types.test_name_short AS type,
@@ -257,6 +259,94 @@ def get_score_card_issues(
     return [row.to_dict() for _, row in results.iterrows()]
 
 
+def get_score_card_issue_reports(selected_issues: list["SelectedIssue"]):
+    profile_ids = []
+    test_ids = []
+    for issue in selected_issues:
+        id_list = profile_ids if issue["issue_type"] == "profile" else test_ids
+        id_list.append(issue["id"])
+
+    schema: str = st.session_state["dbschema"]
+    results = []
+    if profile_ids:
+        profile_query = f"""
+        SELECT
+            results.id::VARCHAR,
+            'hygiene' AS report_type,
+            types.issue_likelihood,
+            runs.profiling_starttime,
+            types.anomaly_name,
+            types.anomaly_description,
+            results.detail,
+            results.schema_name,
+            results.table_name,
+            results.column_name,
+            results.column_type,
+            groups.table_groups_name,
+            results.disposition,
+            results.profile_run_id::VARCHAR,
+            types.suggested_action,
+            results.table_groups_id::VARCHAR,
+            results.anomaly_id::VARCHAR
+        FROM {schema}.profile_anomaly_results results
+        INNER JOIN {schema}.profile_anomaly_types types
+            ON results.anomaly_id = types.id
+        INNER JOIN {schema}.profiling_runs runs
+            ON results.profile_run_id = runs.id
+        INNER JOIN {schema}.table_groups groups
+            ON results.table_groups_id = groups.id
+        WHERE results.id IN ({",".join([f"'{issue_id}'" for issue_id in profile_ids])});
+        """
+        profile_results = db.retrieve_data(profile_query)
+        results.extend([row.to_dict() for _, row in profile_results.iterrows()])
+
+    if test_ids:
+        test_query = f"""
+        SELECT
+            results.id::VARCHAR AS test_result_id,
+            'test' AS report_type,
+            results.result_status,
+            results.test_time,
+            types.test_name_short,
+            types.test_name_long,
+            results.test_description,
+            results.result_measure::NUMERIC(16, 5),
+            types.measure_uom_description,
+            results.threshold_value::NUMERIC(16, 5),
+            types.threshold_description,
+            results.schema_name,
+            results.table_name,
+            results.column_names,
+            groups.table_groups_name,
+            suites.test_suite,
+            types.dq_dimension,
+            CASE
+                WHEN results.result_code <> 1 THEN results.disposition
+                ELSE 'Passed'
+            END as disposition,
+            results.test_run_id::VARCHAR,
+            types.usage_notes,
+            types.test_type,
+            results.auto_gen,
+            results.test_suite_id,
+            results.test_definition_id::VARCHAR as test_definition_id_runtime,
+            results.table_groups_id::VARCHAR,
+            types.id::VARCHAR AS test_type_id
+        FROM {schema}.test_results results
+        INNER JOIN {schema}.test_types types
+            ON (results.test_type = types.test_type)
+        INNER JOIN {schema}.test_suites suites
+            ON (results.test_suite_id = suites.id)
+        INNER JOIN {schema}.table_groups groups
+            ON (results.table_groups_id = groups.id)
+        WHERE results.id IN ({",".join([f"'{issue_id}'" for issue_id in test_ids])});
+        """
+        test_results = db.retrieve_data(test_query)
+        results.extend([row.to_dict() for _, row in test_results.iterrows()])
+
+    return results
+
+
 class ScoreCard(TypedDict):
     project_code: str
     table_group: str
@@ -268,6 +358,11 @@ class ScoreCard(TypedDict):
 class DimensionScore(TypedDict):
     label: str
     score: float
+
+
+class SelectedIssue(TypedDict):
+    id: str
+    issue_type: Literal["profile", "test"]
 
 
 _TABLE_GROUP_SCORES_QUERY = """
