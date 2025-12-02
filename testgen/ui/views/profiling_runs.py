57c8447dfe6841b593b765924e35b32df386cbd2
Aarthy Adityan <aarthy@datakitchen.io>
2025-12-01 22:36:47 -0500

    fix(profiling-runs): error on cancel and improve display

--- testgen/ui/views/profiling_runs.py
+++ testgen/ui/views/profiling_runs.py
@@ -122,7 +122,7 @@ class ProfilingScheduleDialog(ScheduleDialog):
 def on_cancel_run(profiling_run: dict) -> None:
     process_status, process_message = process_service.kill_profile_run(to_int(profiling_run["process_id"]))
     if process_status:
-        ProfilingRun.update_status(profiling_run["profiling_run_id"], "Cancelled")
+        ProfilingRun.cancel_run(profiling_run["id"])
 
     fm.reset_post_updates(str_message=f":{'green' if process_status else 'red'}[{process_message}]", as_toast=True)
 
@@ -171,7 +171,7 @@ def on_delete_runs(project_code: str, table_group_id: str, profiling_run_ids: li
                     if profiling_run.status == "Running":
                         process_status, _ = process_service.kill_profile_run(to_int(profiling_run.process_id))
                         if process_status:
-                            ProfilingRun.update_status(profiling_run.id, "Cancelled")
+                            ProfilingRun.cancel_run(profiling_run.id)
                 ProfilingRun.cascade_delete(profiling_run_ids)
             st.rerun()
         except Exception:
